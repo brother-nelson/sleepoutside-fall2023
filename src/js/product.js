7c2e741111faa57557b34a89877a4ac6c4929467
Brother Nelson <nelsoned@byui.edu>
2023-10-06 20:09:13 +0000

    Team activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,5 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
-import { findProductById } from './productData.mjs';
+import { getParam } from './utils.mjs';
+import productDetails from './productDetails.mjs';
 
-function addProductToCart(product) {
-  const currentCart = getLocalStorage('so-cart') || [];
-  currentCart.push(product);
-  setLocalStorage('so-cart', currentCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const productId = getParam('product');
+productDetails(productId);
